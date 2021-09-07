b63af8936341d2f97e14597332ea379f4e98691d
Nelson Amaris <amigonelson1@hotmail.com>
2021-09-06 19:15:54 -0500

    Se elabora horario

--- src/componentes/Encabezado.jsx
+++ src/componentes/Encabezado.jsx
@@ -9,10 +9,10 @@ export function Encabezado({imagen, titulo, inicioSesion}) {
     return (
       <Fragment>
         <div class="w3-col m3  w3-center">
-          Aqui va la imagen
+          Aquí va la imagen
         </div>
         <div class="w3-col m6  w3-center">
-          Aqui debe retornar el titulo
+          Aquí debe retornar el titulo
         </div>
         <div class="w3-container w3-col m3 w3-center">
           <InicioSesion/>
