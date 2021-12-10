c8628002e5d87216e045dd9fe0bef46b844264c5
LandaAzul <landa98@hotmail.com>
2021-12-09 22:18:13 -0500

    actualizacion de empresa

--- backend/src/routes/empresas.js
+++ backend/src/routes/empresas.js
@@ -6,7 +6,7 @@ const { verifyToken , isUser , isModerator, isAdmin } = require("../middlewares"
 router.route('/')
 const {getEmpresas, createEmpresa, updateEmpresa, deleteEmpresa, getEmpresa} = require('../controllers/empresa.controllers.js')
 router.route('/')
-    .get([verifyToken, isAdmin], getEmpresas)
+    .get(getEmpresas)
     .post([verifyToken, isAdmin], createEmpresa)
 
 router.route('/:id') 
