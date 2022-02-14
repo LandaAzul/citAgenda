a80f6fa805897ade35c008bef9f60d5cb8e5aa01
LandaAzul <landa98@hotmail.com>
2022-02-14 18:01:02 -0500

    Se crea la funcion para comprobar si el usuario esta activo

--- backend/src/routes/administrador.js
+++ backend/src/routes/administrador.js
@@ -1,7 +1,7 @@
 //const router
 const { Router } = require('express');
 const router = Router();
-const {verifyToken, esProfesor, esAdministrador, checkRolesExisted} = require('../middlewares')
+const {verifyToken, esProfesor, esAdministrador, estaActivo, checkRolesExisted} = require('../middlewares')
 //Rutas para los usuarios
 //Importamos el archivo controlador de las rutas con sus funciones 
 const {getUsers, createUser, updateUserId, deleteUserId, getUserId, getUserDocumento, updateUserDocumento, deleteUserDocumento, getUserCodigo, updateUserCodigo, deleteUsercodigo} = require('../controllers/users.controllers.js')
@@ -10,20 +10,20 @@ router.route('/')
     //.get([verifyToken], getUsers)
     .get( getUsers)
     //.post(createUser)
-    .post([verifyToken, esAdministrador, checkRolesExisted], createUser)
+    .post([verifyToken, esAdministrador, estaActivo, checkRolesExisted], createUser)
 router.route('/:id') 
 //para el id
-    .get([verifyToken, esProfesor], getUserId)
-    .put([verifyToken, esAdministrador, checkRolesExisted], updateUserId)
-    .delete([verifyToken, esAdministrador], deleteUserId)
+    .get([verifyToken, estaActivo, esProfesor], getUserId)
+    .put([verifyToken, esAdministrador, estaActivo, checkRolesExisted], updateUserId)
+    .delete([verifyToken, estaActivo, esAdministrador], deleteUserId)
 router.route('/documento/:documento') 
 //para documento
-    .get([verifyToken, esProfesor], getUserDocumento)
-    .put([verifyToken, esAdministrador, checkRolesExisted], updateUserDocumento)
-    .delete([verifyToken, esAdministrador], deleteUserDocumento)
+    .get([verifyToken, estaActivo, esProfesor], getUserDocumento)
+    .put([verifyToken, estaActivo, esAdministrador, checkRolesExisted], updateUserDocumento)
+    .delete([verifyToken, estaActivo, esAdministrador], deleteUserDocumento)
 router.route('/codigo/:codigo') 
 //para codigo
-    .get([verifyToken, esProfesor], getUserCodigo)
-    .put([verifyToken, esAdministrador, checkRolesExisted], updateUserCodigo)
-    .delete([verifyToken, esAdministrador], deleteUsercodigo)
+    .get([verifyToken, estaActivo, esProfesor], getUserCodigo)
+    .put([verifyToken, estaActivo, esAdministrador, checkRolesExisted], updateUserCodigo)
+    .delete([verifyToken, estaActivo, esAdministrador], deleteUsercodigo)
 module.exports = router;
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,7 +1,7 @@
 //const router
 const { Router } = require('express');
 const router = Router();
-const {verifyToken, esProfesor, esAdministrador, esSocio, checkRolesExisted} = require('../middlewares')
+const {verifyToken, esProfesor, esAdministrador, esSocio, checkRolesExisted, estaActivo } = require('../middlewares')
 //Rutas para los usuarios
 //Importamos el archivo controlador de las rutas con sus funciones 
 const {getUsers, createUser, updateUserId, deleteUserId, getUserId, getUserDocumento, updateUserDocumento, deleteUserDocumento, getUserCodigo, updateUserCodigo, deleteUsercodigo, updatePass, updateDataUserId, updateImagenUserId} = require('../controllers/users.controllers.js')
@@ -10,22 +10,22 @@ router.route('/')
     //.get([verifyToken], getUsers)
     .get( getUsers)
     //.post(createUser)
-    .post([verifyToken, esAdministrador, checkRolesExisted], createUser)
+    .post([verifyToken, esAdministrador, estaActivo, checkRolesExisted], createUser)
 router.route('/:id') 
 //para el id
     .get([verifyToken, esSocio], getUserId)
-    .put([verifyToken, esAdministrador, checkRolesExisted], updateUserId)
-    .delete([verifyToken, esAdministrador], deleteUserId)
+    .put([verifyToken, esAdministrador, estaActivo, checkRolesExisted], updateUserId)
+    .delete([verifyToken, estaActivo, esAdministrador], deleteUserId)
 router.route('/documento/:documento') 
 //para documento
-    .get([verifyToken, esAdministrador], getUserDocumento)
-    .put([verifyToken, esAdministrador, checkRolesExisted], updateUserDocumento)
-    .delete([verifyToken, esAdministrador], deleteUserDocumento)
+    .get([verifyToken, esAdministrador, estaActivo], getUserDocumento)
+    .put([verifyToken, esAdministrador, estaActivo, checkRolesExisted], updateUserDocumento)
+    .delete([verifyToken, estaActivo, esAdministrador], deleteUserDocumento)
 router.route('/codigo/:codigo') 
 //para codigo
-    .get([verifyToken, esAdministrador], getUserCodigo)
-    .put([verifyToken, esAdministrador, checkRolesExisted], updateUserCodigo)
-    .delete([verifyToken, esAdministrador], deleteUsercodigo)
+    .get([verifyToken, estaActivo, esAdministrador], getUserCodigo)
+    .put([verifyToken, estaActivo, esAdministrador, checkRolesExisted], updateUserCodigo)
+    .delete([verifyToken, estaActivo, esAdministrador], deleteUsercodigo)
 //para contraseña, imagen y datos
 router.route('/cambiarContra/:id')
     .put([verifyToken, esSocio], updatePass)
