bd55b4c0aca4956bef1c01ef32ea0239a97df681
Nelson Amaris <amigonelson1@hotmail.com>
2022-05-10 19:34:19 -0500

    Se resalta boton de gusrdar datos

--- frontend-pwa/src/componentes/ConfHorario.jsx
+++ frontend-pwa/src/componentes/ConfHorario.jsx
@@ -1407,10 +1407,11 @@ export function ConfHorario() {
                     </div>
                     <div className="w3-col m12 w3-panel w3-center">
                         {franja.length > 0 ? <div>
-                            <button type='submit' style={espacio} className="w3-button w3-indigo w3-border w3-border-black w3-round-large w3-hover-cyan w3-large"
+                            <button type='submit' style={espacio} className="w3-button w3-pink w3-border w3-border-black w3-round-large w3-hover-cyan w3-large"
                                 onClick={crearHorario}>
-                                Crear horario
+                                Crear horario*
                             </button><br></br>
+                            <label className='w3-text-red'>*Recuerda dar clic en "Crear horario" para guardar los cambios</label><br></br>
                             <button type='reset' style={espacio} className="w3-button w3-indigo w3-border w3-border-black w3-round-large w3-hover-blue"
                                 onClick={limpiarTodo} >
                                 Limpiar todo
